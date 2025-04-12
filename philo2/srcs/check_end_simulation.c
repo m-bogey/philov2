36b245113800cf9c9ab9e2de8b5e469c9feb7250
mbogey <mbogey@student.42lyon.fr>
2025-04-12 20:59:20 +0200

    last ?

--- philo2/srcs/check_end_simulation.c
+++ philo2/srcs/check_end_simulation.c
@@ -6,7 +6,7 @@
 /*   By: mbogey <mbogey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 19:18:10 by mbogey            #+#    #+#             */
-/*   Updated: 2025/04/12 19:21:41 by mbogey           ###   ########.fr       */
+/*   Updated: 2025/04/12 20:58:25 by mbogey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,52 @@
 
 static bool	check_philo_die(t_philo *philo);
 
+static int	set_tab(t_table *table, int *i, int *count_meals)
+{
+	if (*count_meals == table->nb_philo)
+	{
+		pthread_mutex_lock(&table->mutex_end.m);
+		table->end_simulation = true;
+		pthread_mutex_unlock(&table->mutex_end.m);
+		print_mutex(table->philos, " all philos full\n");
+		pthread_mutex_lock(&table->mutex_can_write.m);
+		table->can_write = false;
+		pthread_mutex_unlock(&table->mutex_can_write.m);
+	}
+	pthread_mutex_lock(&table->mutex_end.m);
+	if (table->end_simulation == true)
+	{
+		pthread_mutex_unlock(&table->mutex_end.m);
+		return (1);
+	}
+	pthread_mutex_unlock(&table->mutex_end.m);
+	*i = 0;
+	*count_meals = 0;
+	return (0);
+}
+
 void	*check_end_simulation(void *arg)
 {
 	t_table	*table;
 	int		i;
+	int		count_meals;
 
+	count_meals = 0;
 	table = (t_table *)arg;
 	while (wait_for_begin(table) == false)
 		usleep(5);
 	while (1)
 	{
-		pthread_mutex_lock(&table->mutex_end.m);
-		if (table->end_simulation == true)
-		{
-			pthread_mutex_unlock(&table->mutex_end.m);
+		if (set_tab(table, &i, &count_meals) == 1)
 			break ;
-		}
-		pthread_mutex_unlock(&table->mutex_end.m);
-		i = 0;
 		while (i < table->nb_philo)
 		{
 			if (check_philo_die(table->philos + i) == true)
 				break ;
+			pthread_mutex_lock(&table->philos[i].mutex_full.m);
+			if (table->philos[i].full == true)
+				count_meals++;
+			pthread_mutex_unlock(&table->philos[i].mutex_full.m);
 			i++;
 		}
 		usleep(10);
--- philo2/srcs/routine.c
+++ philo2/srcs/routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbogey <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: mbogey <mbogey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 19:19:20 by mbogey            #+#    #+#             */
-/*   Updated: 2025/04/12 19:19:21 by mbogey           ###   ########.fr       */
+/*   Updated: 2025/04/12 20:42:19 by mbogey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void	sleeping(t_philo *philo);
 
 static void	thinking(t_philo *philo)
 {
-/*	long time_think;
+	long	time_think;
 
 	if (philo->table->nb_philo % 2 == 0)
 		time_think = 0;
@@ -25,16 +25,15 @@ static void	thinking(t_philo *philo)
 		time_think = philo->table->time_to_eat * 2
 			- philo->table->time_to_sleep;
 	if (time_think < 0)
-		time_think = 0;*/
+		time_think = 0;
 	print_mutex(philo, " is thinking\n");
-	//usleep_with_check_die(10, philo->table);
+	usleep_with_check_die(time_think, philo->table);
 }
 
 void	*routine(void *arg)
 {
 	t_philo	*philo;
 	bool	end_sim;
-	bool	is_full;
 
 	philo = (t_philo *)arg;
 	while (wait_for_begin(philo->table) == false)
@@ -51,11 +50,6 @@ void	*routine(void *arg)
 		pthread_mutex_unlock(&philo->table->mutex_end.m);
 		if (end_sim == true)
 			break ;
-		pthread_mutex_lock(&philo->mutex_full.m);
-		is_full = philo->full;
-		pthread_mutex_unlock(&philo->mutex_full.m);
-		if (is_full == true)
-			break ;
 		eating(philo);
 		sleeping(philo);
 		thinking(philo);
@@ -63,72 +57,25 @@ void	*routine(void *arg)
 	return (NULL);
 }
 
-// static bool	try_take_forks(t_philo *philo)
-// {
-// 	bool	left;
-// 	bool	right;
-
-// 	left = false;
-// 	right = false;
-// 	pthread_mutex_lock(&philo->left_fork->fork);
-// 	if (philo->left_fork->can_use == true)
-// 	{
-// 		philo->left_fork->can_use = false;
-// 		left = true;
-// 	}
-// 	pthread_mutex_unlock(&philo->left_fork->fork);
-// 	if (left == false)
-// 		return (false);
-
-// 	pthread_mutex_lock(&philo->right_fork->fork);
-// 	if (philo->right_fork->can_use == true)
-// 	{
-// 		philo->right_fork->can_use = false;
-// 		right = true;
-// 	}
-// 	pthread_mutex_unlock(&philo->right_fork->fork);
-
-// 	if (right == false)
-// 	{
-// 		pthread_mutex_lock(&philo->left_fork->fork);
-// 		philo->left_fork->can_use = true;
-// 		pthread_mutex_unlock(&philo->left_fork->fork);
-// 		return (false);
-// 	}
-// 	print_mutex(philo, " has taken a fork\n");
-// 	print_mutex(philo, " has taken a fork\n");
-// 	return (true);
-// }
+static void	to_do_eat(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->mutex_time_last_meal.m);
+	philo->time_last_meal = getime(philo->table);
+	pthread_mutex_unlock(&philo->mutex_time_last_meal.m);
+	print_mutex(philo, " is eating\n");
+	usleep_with_check_die(philo->table->time_to_eat, philo->table);
+	philo->count_meals++;
+	if (philo->table->nb_meals < philo->count_meals
+		&& philo->table->nb_meals > 0)
+	{
+		pthread_mutex_lock(&philo->mutex_full.m);
+		philo->full = true;
+		pthread_mutex_unlock(&philo->mutex_full.m);
+	}
+}
 
 static void	eating(t_philo *philo)
 {
-	// if (try_take_forks(philo) == false)
-	// 	return ;
-	// pthread_mutex_lock(&philo->mutex_time_last_meal.m);
-	// philo->time_last_meal = getime(philo->table);
-	// pthread_mutex_unlock(&philo->mutex_time_last_meal.m);
-
-	// print_mutex(philo, " is eating\n");
-	// usleep_with_check_die(philo->table->time_to_eat, philo->table);
-	// philo->count_meals++;
-	// if (philo->table->nb_meals < philo->count_meals
-	// 	&& philo->table->nb_meals > 0)
-	// {
-	// 	pthread_mutex_lock(&philo->mutex_full.m);
-	// 	philo->full = true;
-	// 	pthread_mutex_unlock(&philo->mutex_full.m);
-	// }
-	// pthread_mutex_lock(&philo->left_fork->fork);
-	// philo->left_fork->can_use = true;
-	// pthread_mutex_unlock(&philo->left_fork->fork);
-
-	// pthread_mutex_lock(&philo->right_fork->fork);
-	// philo->right_fork->can_use = true;
-	// pthread_mutex_unlock(&philo->right_fork->fork);
-	
-	// while (philo->left_fork->can_use != true)
-	// 	usleep(10);
-	
 	pthread_mutex_lock(&philo->left_fork->fork);
 	if (philo->left_fork->can_use == true)
 	{
@@ -139,21 +86,7 @@ static void	eating(t_philo *philo)
 		{
 			philo->right_fork->can_use = false;
 			print_mutex(philo, " has taken a fork\n");
-			
-			pthread_mutex_lock(&philo->mutex_time_last_meal.m);
-			philo->time_last_meal = getime(philo->table);
-			pthread_mutex_unlock(&philo->mutex_time_last_meal.m);
-			print_mutex(philo, " is eating\n");
-			usleep_with_check_die(philo->table->time_to_eat, philo->table);
-			philo->count_meals++;
-			if (philo->table->nb_meals < philo->count_meals
-				&& philo->table->nb_meals > 0)
-			{
-				pthread_mutex_lock(&philo->mutex_full.m);
-				philo->full = true;
-				pthread_mutex_unlock(&philo->mutex_full.m);
-			}
-
+			to_do_eat(philo);
 			philo->right_fork->can_use = true;
 		}
 		pthread_mutex_unlock(&philo->right_fork->fork);
